4665a5fc2b61335e70acecdff207e3c1e4648b16
Andrey Karyaka <133392737+akaryaka@users.noreply.github.com>
2023-08-09 19:03:05 +0600

    add new  photo, fixed theme

--- js/themeToggle.js
+++ js/themeToggle.js
@@ -1,7 +1,6 @@
 
 const btnTheme = document.querySelector('#theme-toggle-btn');
-
-// const body = document.querySelector('body');
+const body = document.querySelector('.body');
 const wrappers = document.querySelectorAll('.wrapper');
 const blocks = document.querySelectorAll('.block');
 const text = document.querySelectorAll('.text');
@@ -10,6 +9,7 @@ const title2Before = document.querySelectorAll('.title-2');
 btnTheme.addEventListener('click', function () {
   wrappers.forEach(function(wrapper) {
     wrapper.classList.toggle('wrapper_dark')
+    body.classList.toggle('wrapper_dark')
   })
 
   blocks.forEach(function(block) {
