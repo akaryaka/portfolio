bb61d09d6885184d30b542ce23ff29463f53de08
Andrey Karyaka <133392737+akaryaka@users.noreply.github.com>
2023-08-17 18:06:03 +0600

    refactoring js, fixed bugs

--- js/themeToggle.js
+++ js/themeToggle.js
@@ -1,26 +1,20 @@
 
 const btnTheme = document.querySelector('#theme-toggle-btn');
-
-// const body = document.querySelector('body');
+const body = document.querySelector('body');
 const wrappers = document.querySelectorAll('.wrapper');
 const blocks = document.querySelectorAll('.block');
 const text = document.querySelectorAll('.text');
 const title2Before = document.querySelectorAll('.title-2');
 
 btnTheme.addEventListener('click', function () {
-  wrappers.forEach(function(wrapper) {
-    wrapper.classList.toggle('wrapper_dark')
-  })
-
-  blocks.forEach(function(block) {
-    block.classList.toggle('block_dark')
-  })
-
-  text.forEach(function(txt) {
-    txt.classList.toggle('_text_dark');
-  })
-
-  title2Before.forEach(function(item){
-    item.classList.toggle('title-2_dark');
-  })
+  body.classList.toggle('wrapper_dark');
+  toggleClass(blocks, 'block', 'block_dark');
+  toggleClass(text, 'txt', '_text_dark');
+  toggleClass(title2Before, 'item', 'title-2_dark');
 })
+
+function toggleClass(array, elem, classBlock) {
+  array.forEach(function(elem) {
+    elem.classList.toggle(classBlock);
+  })
+}
